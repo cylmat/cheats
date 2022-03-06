94f68cfe9d75bcca2a05fd1b83835c6ec15d01dc
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2022-03-06 20:44:23 +0100

    Update and rename Online-bash-sample.sh to onliner-bash-sample.sh

--- onliner-bash-sample.sh
+++ onliner-bash-sample.sh
@@ -1,3 +1,8 @@
 # Sample
 
 tail file | awk '{d=$1} sub(/.*{/,""){$0="{\"date\": \""d"\", " $0} 1' | jq -r '.date + " " + .log'
+
+---
+**@ref**: 
+- https://www.bashoneliners.com
+- https://www.commandlinefu.com
