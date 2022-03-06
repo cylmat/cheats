e0bdbcc1ab5d5184bfe0d84c34faa2338864d46b
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2022-03-06 20:51:03 +0100

    Update onliner-bash-sample.sh

--- onliner-bash-sample.sh
+++ onliner-bash-sample.sh
@@ -2,7 +2,7 @@
 
 tail file | awk '{d=$1} sub(/.*{/,""){$0="{\"date\": \""d"\", " $0} 1' | jq -r '.date + " " + .log'
 
----
+# ---
 # @ref: 
 - https://www.bashoneliners.com
 - https://www.commandlinefu.com
